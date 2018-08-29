a951fa435457dcedf858b45a52ca5be608c20bd3
Park Sung Min <smp1991@naver.com>
2018-08-29 08:31:43 +0000

    Faker module

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 from flask import Flask, render_template, request
 import random
 import csv
+from faker import Faker
 
 ## names = []
 
 app = Flask(__name__)
+fake = Faker('ko_KR')
 
 @app.route("/")
 def index():
@@ -36,5 +38,10 @@ def admin():
     rr = csv.reader(f)
     names = rr
     return render_template('admin.html', names=names)
-    
+
+@app.route("/ffaker")
+def ffaker():
+    name = fake.name()
+    return render_template('ffaker.html', name=name)
+
 app.run(host='0.0.0.0', port='8080', debug=True)
